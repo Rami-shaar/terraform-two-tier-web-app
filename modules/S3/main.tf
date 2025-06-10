97bb68ad71bebc6aba3be1bdbabc5f6f9473b7e8
Rami <rami.h.alshaar@gmail.com>
2025-06-10 03:48:00 +0100

    Update main.tf

--- modules/S3/main.tf
+++ modules/S3/main.tf
@@ -1,6 +1,11 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket for Terraform remote backend"
+  type        = string
+}
+
 resource "aws_s3_bucket" "my_bucket" {
-  bucket         = var.bucket_name
-  force_destroy  = true
+  bucket        = var.bucket_name
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
@@ -18,13 +23,27 @@ resource "aws_s3_bucket_lifecycle_configuration" "bucket_lifecycle" {
     id     = "expire-old-versions"
     status = "Enabled"
 
-   filter {
-  prefix = ""
-}
+    filter {
+      prefix = ""
+    }
 
-
-   noncurrent_version_expiration {
+    noncurrent_version_expiration {
       noncurrent_days = 30
     }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-lock-id"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Lock Table"
   }
 }
