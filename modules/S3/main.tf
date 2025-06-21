20f91f7d61ee58bb09b1a9b29ab16d0a7ad7e7a0
Rami <rami.h.alshaar@gmail.com>
2025-06-21 01:10:55 +0100

    Update main.tf

--- modules/S3/main.tf
+++ modules/S3/main.tf
@@ -27,18 +27,3 @@ resource "aws_s3_bucket_lifecycle_configuration" "bucket_lifecycle" {
     }
   }
 }
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-lock-id"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "Terraform Lock Table"
-  }
-}
