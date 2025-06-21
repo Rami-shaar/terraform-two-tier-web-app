9a1ff189729cdbbd5bdfe9a9b18929755a01b633
Rami <rami.h.alshaar@gmail.com>
2025-06-21 01:16:44 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "alb_dns_name" {
   value = module.compute.alb_dns_name
 }
+
+output "app_s3_bucket_name" {
+  value       = module.s3.s3_bucket_name
+  description = "The name of the app asset S3 bucket"
+}
